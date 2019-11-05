ec8c41b4f20122834d80dc90683e8b78ceffd935
ADAM VARDEN - STUDENT <G00359605@gmit.ie>
2019-11-05 13:00:36 +0000

    first commit

---
+++ lab8/Assets/Script/CubeMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMovement : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    private float moveSpeed;
+    private float dirX, dirZ;
+    // Start is called before the first frame update
+    void Start()
+    {
+         moveSpeed = 3f;
+         rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dirX = Input.GetAxis("Horizontal") * moveSpeed;
+        dirZ = Input.GetAxis("Vertical") * moveSpeed;
+
+//  if (Input.GetKey(KeyCode.W))
+//         {
+//             transform.Translate(Vector3.forward * Time.deltaTime);
+//         }
+
+//         if (Input.GetKey(KeyCode.S))
+//         {
+//             transform.Translate(Vector3.back * Time.deltaTime);
+//         }
+
+//          if (Input.GetKey(KeyCode.A))
+//         {
+//              transform.Translate(Vector3.left * Time.deltaTime);
+//         }
+
+//         if (Input.GetKey(KeyCode.D))
+//         {
+//             transform.Translate(Vector3.right * Time.deltaTime);
+//          }       
+         }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector3(dirX,rb.velocity.y,dirZ);
+    }
+}
